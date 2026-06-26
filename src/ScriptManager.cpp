42ce59462b2ca58bc6ddd02f44cdc220230dd6d9
doyaGu <doyaGu@users.noreply.github.com>
2026-06-26 18:52:33 -0400

    Move diagnostics facade methods

--- src/ScriptManager.cpp
+++ src/ScriptManager.cpp
@@ -1,7 +1,5 @@
 #include "ScriptManager.h"
 
-#include <string>
-
 #ifndef CKAS_BUILD_SELF_TESTS
 #define CKAS_BUILD_SELF_TESTS 0
 #endif
@@ -209,47 +207,6 @@ int ScriptManager::Shutdown() {
 
     m_Flags &= ~AS_INITED;
     return 0;
-}
-
-CKAngelScriptResult ScriptManager::MakeResult(CKAS_STATUS status,
-                                            int angelScriptCode,
-                                            const std::string &errorMessage,
-                                            const std::string &stackTrace,
-                                            const std::vector<CapturedScriptMessage> *compilerMessages) {
-    return m_Diagnostics.MakeResult(status, angelScriptCode, errorMessage, stackTrace, compilerMessages);
-}
-
-CKAS_STATUS ScriptManager::StoreResult(CKAngelScriptResult *out,
-                                              CKAS_STATUS status,
-                                              int angelScriptCode,
-                                              const std::string &errorMessage,
-                                              const std::string &stackTrace,
-                                              const std::vector<CapturedScriptMessage> *compilerMessages) {
-    return m_Diagnostics.StoreResult(out, status, angelScriptCode, errorMessage, stackTrace, compilerMessages);
-}
-
-const CKAngelScriptResult *ScriptManager::GetLastResult() const {
-    return m_Diagnostics.GetLastResult();
-}
-
-CKAS_STATUS ScriptManager::StoreApiResult(CKAngelScriptResult *out,
-                                          CKAS_STATUS status,
-                                          int angelScriptCode,
-                                          const char *errorMessage,
-                                          const char *stackTrace) {
-    return StoreResult(out,
-                       status,
-                       angelScriptCode,
-                       std::string(errorMessage ? errorMessage : ""),
-                       std::string(stackTrace ? stackTrace : ""));
-}
-
-void ScriptManager::BeginScriptMessageCapture() {
-    m_Diagnostics.BeginScriptMessageCapture();
-}
-
-std::string ScriptManager::EndScriptMessageCapture(std::vector<CapturedScriptMessage> *messages) {
-    return m_Diagnostics.EndScriptMessageCapture(messages);
 }
 
 CKERROR ScriptManager::ResolveScriptFileName(XString &filename) {
